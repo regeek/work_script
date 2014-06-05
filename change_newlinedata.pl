736f27564c7010d85ae85355c7d2eea593f2b5b7
regeek <rebellion.geek@gmail.com>
2014-06-05 22:30:40 +0900

    update

--- change_newlinedata.pl
+++ change_newlinedata.pl
@@ -6,41 +6,35 @@ use warnings;
 ####################
 #main
 ####################
-my $data_length = 10;
-my @bin_array = ();
-my $cnt_array = 0;
+my $data_length = 0;
+my @array_bin = ();
 my $msg_01 = "改行データに変換しました";
-my $msg_02 = "改行データに変換できません";
-my $msg_03 = "提供データの改行が存在している可能性があります";
-my $msg_04 = "引数を指定して下さい";
-my $msg_05 = "ファイルが存在しません";
+my $msg_02 = "引数を指定して下さい";
+my $msg_03 = "ファイルが存在しません";
 
-my $num_argv = @ARGV;
-if($num_argv > 0){
+if(@ARGV){
     foreach my $file(@ARGV){
         if( -f $file){
-            my $filesize = `ls -l $file | awk '{ print \$5 }'`;
-            #shellの結果に改行が含まれるため、改行を削除
-            chomp($filesize);
+            print "何バイトで折り返しますか？ byte = ";
+            $data_length = <STDIN>;
+            chomp($data_length);
 
-            if( ($filesize % $data_length) == 0 ){
-                ReadFile($file);
-                $cnt_array = @bin_array;
-                WriteFile($file);
+            if($data_length !~ /^\d+$/){
+                print "数値を入力して下さい\n";
+                exit(0);
+            }
 
-                print "FILENAME:$file, FILESIZE:$filesize byte, $msg_01\n";
-            }
-            else{
-                print "FILENAME:$file, FILESIZE:$filesize byte, $msg_02, $msg_03\n";
-            }
+            ReadFile($file);
+            WriteFile($file);
+            print "FILENAME:$file, $msg_01\n";
         }
         else{
-            print "FILENAME:$file, $msg_05\n";
+            print "FILENAME:$file, $msg_03\n";
         }
     }
 }
 else{
-    print "$msg_04\n";
+    print "$msg_02\n";
 }
 
 ####################
@@ -55,9 +49,8 @@ sub ReadFile{
     binmode IN;
 
     while (read IN, $buf, $data_length) {
-        my ( $data) = unpack "a10", $buf;
-        $bin_array[$cnt] = $data;
-
+        my $data = unpack "a$data_length", $buf;
+        $array_bin[$cnt] = $data;
         $cnt++;
     }
 
@@ -72,9 +65,9 @@ sub WriteFile{
     open(IN,"+>",$file) or die "$!";
     binmode IN;
 
-    #改行データに書き換え
-    for(my $i=0;$i<$cnt_array;$i++){
-        $buf = pack "a10 a1", $bin_array[$i], $newline;
+    #改行挿入
+    foreach my $data(@array_bin){
+        $buf = pack "a$data_length  a1", $data, $newline;
         print IN $buf;
     }
 
