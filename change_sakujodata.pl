ad33418274b88f5a5652c047455a1690a4b130b7
regeek <rebellion.geek@gmail.com>
2014-06-17 22:34:23 +0900

    2nd add

--- change_sakujodata.pl
+++ change_sakujodata.pl
@@ -9,11 +9,12 @@ use warnings;
 my $data_length = 10;
 my @array_bin = ();
 my $cnt_array = 0;
-my $msg_01 = "削除データに変換しました";
+my $msg_01 = "削除データ（定期）に変換しました";
 my $msg_02 = "削除データに変換できません";
 my $msg_03 = "提供データの改行を削除していない可能性があります";
 my $msg_04 = "引数を指定して下さい";
 my $msg_05 = "ファイルが存在しません";
+my $msg_06 = "削除データ（初期）に変換しました";
 
 my $num_argv = @ARGV;
 if($num_argv > 0){
@@ -23,16 +24,20 @@ if($num_argv > 0){
             #shellの結果に改行が含まれるため、改行を削除
             chomp($filesize);
 
-            if( ($filesize % $data_length) == 0 &&
-                 $file =~ FFFFF[2|3|5]11 &&
-                 $file =~ FFFFF001 ){
-                ReadFile($file);
+            if( ($filesize % $data_length) == 0 && ($file =~ /^FFFFF[2|3|5]11/ ) ){
+                ReadFile_Teiki($file);
                 $cnt_array = @array_bin;
-                WriteFile($file);
+                WriteFile_Teiki($file);
 
                 print "FILENAME:$file, FILESIZE:$filesize byte, $msg_01\n";
             }
-            else{
+            elsif( ($filesize % $data_length) == 0 && ($file =~ /^FFFFF001/) ){
+                ReadFile_Shoki($file);
+                $cnt_array = @array_bin;
+                WriteFile_Shoki($file);
+
+                print "FILENAME:$file, FILESIZE:$filesize byte, $msg_06\n";
+            }else{
                 print "FILENAME:$file, FILESIZE:$filesize byte, $msg_02, $msg_03\n";
             }
         }
@@ -48,7 +53,7 @@ else{
 ####################
 #subroutine
 ####################
-sub ReadFile{
+sub ReadFile_Teiki{
     my $file = $_[0];
     my $cnt = 0;
     my $buf = "";
@@ -69,7 +74,7 @@ sub ReadFile{
     close(IN);
 }
 
-sub WriteFile{
+sub WriteFile_Teiki{
     my $file = $_[0];
     my $syurui_Z = "Z";
     my $buf = "";
@@ -86,3 +91,43 @@ sub WriteFile{
     close(IN);
 }
 
+sub ReadFile_Shoki{
+    my $file = $_[0];
+    my $cnt = 0;
+    my $buf = "";
+
+    open(IN,"+<",$file) or die "$!";
+    binmode IN;
+
+    while (read IN, $buf, $data_length) {
+        #データ種類、データ種別、データ種類以外のデータで分割
+        #データ種類以外のデータのみ使用
+        my ( $syurui, $data1, $shubetu, $data2) = unpack "a1 a4 a1 a4", $buf;
+        $array_bin[$cnt][0] = $syurui;
+        $array_bin[$cnt][1] = $data1;
+        $array_bin[$cnt][2] = $shubetu;
+        $array_bin[$cnt][3] = $data2;
+
+        $cnt++;
+    }
+
+    close(IN);
+}
+
+sub WriteFile_Shoki{
+    my $file = $_[0];
+    my $syurui_Z = "Z";
+    my $shubetu_Y = "Y";
+    my $buf = "";
+
+    open(IN,"+>",$file) or die "$!";
+    binmode IN;
+
+    #削除データに書き換え
+    for(my $i=0;$i<$cnt_array;$i++){
+        $buf = pack "a1 a4 a1 a4", $syurui_Z, $array_bin[$i][1], $shubetu_Y, $array_bin[$i][3];
+        print IN $buf;
+    }
+
+    close(IN);
+}
